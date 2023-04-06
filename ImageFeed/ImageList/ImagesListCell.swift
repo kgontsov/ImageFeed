f2c46a27db7eb1f04ff4b9dfba941d491a4f339e
kgontsov <mail@kgontsov.ru>
2023-04-06 18:32:23 +0300

    по заданию

---
+++ ImageFeed/ImageList/ImagesListCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Kirill Gontsov on 06.04.2023.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+}
---
+++ ImageFeed/ImageList/ImagesListViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Kirill Gontsov on 23.03.2023.
+//
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+
+    @IBOutlet private var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func configCell(for cell: ImagesListCell) {
+        cell.subviews[0].subviews[0]
+    }
+    
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+        
+        guard let imageListCell = cell as? ImagesListCell else {
+            return UITableViewCell()
+        }
+        
+        configCell(for: imageListCell)
+        return imageListCell
+    }
+}
+
+
