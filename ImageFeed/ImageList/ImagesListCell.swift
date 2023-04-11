6c5a7aaab885e472a64a2ea431f622877ff74ece
kgontsov <mail@kgontsov.ru>
2023-04-11 11:14:29 +0300

    констрейны

--- ImageFeed/ImageList/ImagesListCell.swift
+++ ImageFeed/ImageList/ImagesListCell.swift
@@ -9,4 +9,10 @@ import UIKit
 
 final class ImagesListCell: UITableViewCell {
     static let reuseIdentifier = "ImagesListCell"
+    
+    
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
+    
 }
