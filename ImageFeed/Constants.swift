c3e897e7aeacf58f6dda68bcaca2fc3f97f764bd
kgontsov <mail@kgontsov.ru>
2023-07-04 13:35:24 +0300

    Начало 10го спринта+constants

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Kirill Gontsov on 04.07.2023.
+//
+
+import Foundation
+
+let AccessKey = "Cmk-qBrcPR6pjLRzexDaJYjuh6_SxvFdUhOAX9o5U4s"
+let SecretKey = "gOg4oImbpQd9BJ4ff_TOrVSCCfUm7haxY9J-vTLMQ3s"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
